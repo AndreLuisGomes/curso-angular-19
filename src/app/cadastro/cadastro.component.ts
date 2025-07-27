22aeffe3e77a5a1e84513e82e982942d9aed5806
AndreLuisGomes <andreluisgomes09@gmail.com>
2025-07-27 00:21:35 -0300

    feat: completando formulario e adicionando classe de cliente

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+    FlexLayoutModule,
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
-
+  cliente: Cliente= Cliente.newCliente(); 
 }
